4956643a65d6493b80451ee29e2a04b068de3891
Virginia Marc <vmarc4@hccfl.edu>
2026-05-25 05:29:15 -0400

    added favicon icon to assets folder

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,9 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/5.2.3/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico?v=3">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png?v=3">
+
         <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" ></script>
     </head>
     
